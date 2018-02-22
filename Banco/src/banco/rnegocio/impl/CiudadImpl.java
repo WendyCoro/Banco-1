ef71e0ecd42e443092a9cc0957d9bce98cf36540
WilliamPaucar <>
2018-02-22 11:56:48 -0500

    Actualizacion Implementaciones

--- Banco/src/banco/rnegocio/impl/CiudadImpl.java
+++ Banco/src/banco/rnegocio/impl/CiudadImpl.java
@@ -5,10 +5,126 @@
  */
 package banco.rnegocio.impl;
 
+import banco.accesodatos.*;
+import banco.rnegocio.entidades.*;
+import banco.rnenogio.dao.ICiudad;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author WILCXMAC
  */
-public class CiudadImpl {
+public class CiudadImpl implements ICiudad{
     
+   @Override
+    public int insertar(Ciudad ciudad) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "insert into ciudad  values (?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, ciudad.getId_ciudad()));
+        lstPar.add(new Parametro(2, ciudad.getNombre()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int modificar(Ciudad ciudad) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "update  ciudad set id_ciudad=?,nombre=? where id_ciudad=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, ciudad.getId_ciudad()));
+        lstPar.add(new Parametro(2, ciudad.getNombre()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int eliminar(Ciudad ciudad) throws Exception {
+        int numFilasAfectadas = 0;
+         String sql = "DELETE * FROM ciudad  where id_ciudad=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, ciudad.getId_ciudad()));       
+        lstPar.add(new Parametro(1, ciudad.getNombre()));       
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public Ciudad obtener(int codigo) throws Exception {
+        Ciudad ciudad = null;
+         String sql = "select id_ciudad, nombre from ciudad where id_ciudad=?"; 
+          List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, codigo));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, lstPar);            
+            while(rst.next()){
+                ciudad= new Ciudad();
+                ciudad.setId_ciudad(rst.getInt(1));
+                ciudad.setNombre(rst.getString(2));            
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return ciudad;
+    }
+
+    @Override
+    public List<Ciudad> obtener() throws Exception {
+        List<Ciudad> lista = new ArrayList<>();
+         String sql = "select id_ciudad, nombre from ciudad";        
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, null);
+            Ciudad ciudad= null;
+            while(rst.next()){
+                ciudad= new Ciudad();
+                ciudad.setId_ciudad(rst.getInt(1));
+                ciudad.setNombre(rst.getString(2));
+                lista.add(ciudad);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+          return lista ;
+    }  
 }
--- Banco/src/banco/rnegocio/impl/ClienteImpl.java
+++ Banco/src/banco/rnegocio/impl/ClienteImpl.java
@@ -5,10 +5,162 @@
  */
 package banco.rnegocio.impl;
 
+import banco.accesodatos.*;
+import banco.rnegocio.entidades.*;
+import banco.rnenogio.dao.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author WILCXMAC
  */
-public class ClienteImpl {
+public class ClienteImpl implements ICliente{
     
+    
+     @Override
+     
+     public int ingresar(Cliente cliente) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "insert into cliente  values "
+                + "(?,?,?,?,?,?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, cliente.getCodigo_cliente()));
+        lstPar.add(new Parametro(2, cliente.getCedula()));
+        lstPar.add(new Parametro(3, cliente.getNombres()));
+        lstPar.add(new Parametro(4, cliente.getApellidos()));
+        lstPar.add(new Parametro(5, cliente.getCelular()));
+        lstPar.add(new Parametro(6, cliente.getEmail()));
+        lstPar.add(new Parametro(7, cliente.getPrestamo().getId_prestamo()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int modificar(Cliente cliente) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "UPDATE cliente"
+                + "   SET codigo_cliente=?, cedula=?, nombres=?, apellidos=?, celular=?, "
+                + "email=?,cod_prestamo"
+                + " where codigo_cliente=?";
+        List<Parametro> lstPar = new ArrayList<>();
+       lstPar.add(new Parametro(1, cliente.getCodigo_cliente()));
+        lstPar.add(new Parametro(2, cliente.getCedula()));
+        lstPar.add(new Parametro(3, cliente.getNombres()));
+        lstPar.add(new Parametro(4, cliente.getApellidos()));
+        lstPar.add(new Parametro(5, cliente.getCelular()));
+        lstPar.add(new Parametro(6, cliente.getEmail()));
+        lstPar.add(new Parametro(7, cliente.getPrestamo().getId_prestamo()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int eliminar(Cliente cliente) throws Exception {
+        int numFilasAfectadas = 0;
+         String sql = "DELETE FROM cliente  where codigo_cliente=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, cliente.getCodigo_cliente()));       
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public Cliente obtener(int codigo) throws Exception {
+        Cliente cliente = null;
+        String sql = "SELECT *   FROM cliente where codigo_cliente=?;";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, codigo));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, lstPar);
+            while (rst.next()) {
+                cliente = new Cliente();
+                cliente.setCodigo_cliente(rst.getInt(1));
+                cliente.setCedula(rst.getInt(2));
+                cliente.setNombres(rst.getString(3));
+                cliente.setApellidos(rst.getString(4));
+                cliente.setCelular(rst.getInt(5));
+                cliente.setEmail(rst.getString(6));
+
+                IPrestamo prestamodao = new PrestamoImpl();
+                Prestamo prestamo = prestamodao.obtener(rst.getInt(7));
+                cliente.setPrestamo(prestamo);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return cliente;
+    }
+
+    @Override
+    public List<Cliente> obtener() throws Exception {
+        List<Cliente> lista = new ArrayList<>();
+         String sql = "SELECT *   FROM cliente ";        
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, null);
+            Cliente cliente=null;
+            while (rst.next()) {
+                cliente = new Cliente();
+                cliente.setCodigo_cliente(rst.getInt(1));
+                cliente.setCedula(rst.getInt(2));
+                cliente.setNombres(rst.getString(3));
+                cliente.setApellidos(rst.getString(4));
+                cliente.setCelular(rst.getInt(5));
+                cliente.setEmail(rst.getString(6));
+
+                IPrestamo prestamodao = new PrestamoImpl();
+                Prestamo prestamo = prestamodao.obtener(rst.getInt(7));
+                cliente.setPrestamo(prestamo);
+                lista.add(cliente);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return lista;
+    }
 }
